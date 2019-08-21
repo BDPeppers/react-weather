83661f989cc63f96e2bcbb10179377af1844809c
BDPeppers <peppersbre@yahoo.com>
2019-08-21 11:25:17 -0400

    Updates

---
+++ precache-manifest.e42209813990dc567a3cdc0ddcce4e7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c2b976dbdd3d71febbc609770eb4b4",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "45fd86ae8e78a258896e",
+    "url": "/react-weather/static/css/main.2472d28e.chunk.css"
+  },
+  {
+    "revision": "ef18b9f5e00081bc1400",
+    "url": "/react-weather/static/js/2.f9508401.chunk.js"
+  },
+  {
+    "revision": "45fd86ae8e78a258896e",
+    "url": "/react-weather/static/js/main.e9e1ccff.chunk.js"
+  },
+  {
+    "revision": "e61e452087af7072f6e9",
+    "url": "/react-weather/static/js/runtime~main.2302d1fd.js"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/react-weather/static/media/clear-day.7f93fbdb.svg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/react-weather/static/media/clear-night.a3f3f38d.svg"
+  },
+  {
+    "revision": "80a5d865c63ba642a927daba54438305",
+    "url": "/react-weather/static/media/cloudy-day.80a5d865.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/react-weather/static/media/cloudy-night.ef1c6aa1.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/react-weather/static/media/rainy.d235ab65.svg"
+  },
+  {
+    "revision": "67d63a14dbbef1d431f1851da197e0ae",
+    "url": "/react-weather/static/media/snowy.67d63a14.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/react-weather/static/media/thunder.7142c78b.svg"
+  }
+]);
